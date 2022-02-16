bc3409da801b705d24863b1c86abf14a0851f8d8
Bruno Hammes <bruno.hammes22@gmail.com>
2022-02-16 05:18:29 -0400

    Adicionar um auxilio full_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  # Retorna o título completo por página.
+  def full_title(page_title = '')
+    base_title = 'Aplicativo de amostra do tutorial Ruby on Rails'
+    page_title.empty? ? base_title : page_title + ' | ' + base_title
+  end
 end
